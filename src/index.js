3551d2584a6ee6050cddfe1a223c386f624bebf6
Kirill Sakharov <kirillsakharov@MacBook-Pro-16-inch-2021.local>
2023-08-18 22:49:49 +0100

    uploading images working

--- src/index.js
+++ src/index.js
@@ -12,6 +12,8 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+app.use("/uploads", express.static("./uploads"));
+
 mongoose.connect(
   "mongodb+srv://kirillsakharov:Blockchain123@cluster1.rxxvrjb.mongodb.net/",
   {
@@ -28,9 +30,77 @@ const UserSchema = new mongoose.Schema({
   password: String,
   address: String,
   mnemonic: String,
+  profilePic: String,
 });
 
 const User = mongoose.model("User", UserSchema);
+
+const multer = require("multer");
+
+// Multer configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
+
+// Endpoint for uploading the profile picture
+app.post("/uploadProfilePic", upload.single("profilePic"), async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).send({ message: "User not found" });
+    }
+
+    user.profilePic = req.file.path;
+    await user.save();
+
+    res.status(200).json({
+      message: "Profile picture uploaded successfully!",
+      imageUrl: `/uploads/${req.file.filename}`, // Provide a public URL
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+app.get("/getUserProfilePic/:username", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ profilePic: user.profilePic });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
@@ -61,6 +131,7 @@ app.post("/register", async (req, res) => {
     password: hashedPassword,
     address,
     mnemonic,
+    profilePic: null,
   });
 
   try {
